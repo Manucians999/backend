8e0391e4c08073599a18a1e364c9c2b65712efb5
luuvanhung <lvhdev99@gmail.com>
2022-01-19 21:49:48 +0700

    update code

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,53 +1,53 @@
-const md5 = require('md5');
+const md5 = require("md5");
 
-const Product = require('../models/product.model');
-const User = require('../models/user.model');
+const Product = require("../models/product.model");
+const User = require("../models/user.model");
 
 module.exports = {
   index: async (req, res) => {
-    res.render('home/index', {
+    res.render("home/index", {
       data: await Product.find(),
       session: req.signedCookies.ssid,
-      user: await User.find({id: req.signedCookies.ssid}),
-      title: 'Home Page - Website about the House'
-    })
+      user: await User.find({ _id: req.params.id }),
+      title: "Home Page - Website about the House",
+    });
   },
 
   about: (req, res) => {
-    res.render('home/about', {
-      title: 'About Page - Website about the House'
+    res.render("home/about", {
+      title: "About Page - Website about the House",
     });
   },
 
   logout: (req, res) => {
-    if(req.signedCookies.ssid) {
-      if(req.signedCookies.ssaid) {
-        res.clearCookie('ssaid');
+    if (req.signedCookies.ssid) {
+      if (req.signedCookies.ssaid) {
+        res.clearCookie("ssaid");
       }
-      res.clearCookie('ssid');
-      res.redirect('/');
+      res.clearCookie("ssid");
+      res.redirect("/");
     }
   },
 
   register: (req, res) => {
-    res.render('home/register', {
-      title: 'Register Page - Website about the House'
+    res.render("home/register", {
+      title: "Register Page - Website about the House",
     });
   },
   postRegister: (req, res) => {
-    User.create(
-      {
-        username: req.body.username,
-        fullname: req.body.fullname,
-        address: req.body.address,
-        phone: req.body.phone,
-        email: req.body.email,
-        password: md5(req.body.password)
-      }
-    ).then(() =>{
-      res.redirect('/auth/login');
-    }).catch((err) => {
-      console.log(err);
+    User.create({
+      username: req.body.username,
+      fullname: req.body.fullname,
+      address: req.body.address,
+      phone: req.body.phone,
+      email: req.body.email,
+      password: md5(req.body.password),
     })
+      .then(() => {
+        res.redirect("/auth/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
-}
+};
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const productController = require('../controllers/product.controller');
+const productController = require("../controllers/product.controller");
 
 const router = express.Router();
 
-router.get('/id=:id', productController.get_product_by_id);
+router.get("/id=:id", productController.get_product_by_id);
 
-router.get('/', productController.index);
+router.get("/", productController.index);
 
-router.get('/load=:load', productController.loadProducts)
+router.get("/load=:load", productController.loadProducts);
 
 module.exports = router;
